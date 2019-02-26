092947e5bf7e0a36d92d90c82ecb44aa6f082ebd
Kevin Talmadge <kevin@eluv.io>
2019-02-26 15:16:25 -0800

    Initial commit

---
+++ src/components/AnimatedIcons.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../stylesheets/animated-icons/animated-icons.scss";
+
+export const BallPulse = () => {
+  return (
+    <div className="animated-icon la-ball-pulse la-dark">
+      <div/>
+      <div/>
+      <div/>
+    </div>
+  );
+};
+
+export const BallClipRotate = () => {
+  return (
+    <div className="animated-icon la-ball-clip-rotate la-dark">
+      <div></div>
+    </div>
+  );
+};
