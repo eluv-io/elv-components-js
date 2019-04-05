e94d92546c67f1a36f8b6553850f6ede4b4e89db
Kevin Talmadge <kevin@eluv.io>
2019-04-05 14:29:09 -0700

    Add alternate fallback image to ImageIcon and CroppedImage

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -5,17 +5,17 @@ import React from "react";
 import Link from "react-router-dom/es/Link";
 import {onEnterPressed} from "../utils/Events";
 
-export const CroppedIcon = ({icon, title, className, iconClassName}) => {
+export const CroppedIcon = ({icon, alternateIcon, title, className, iconClassName}) => {
   return (
     <div className={className || ""}>
       <div className="-elv-cropped-icon">
-        <ImageIcon icon={icon} title={title} className={iconClassName || ""} />
+        <ImageIcon icon={icon} alternateIcon={alternateIcon} title={title} className={iconClassName || ""} />
       </div>
     </div>
   );
 };
 
-export const CroppedIconWithAction = ({icon, title, actionText, onClick, className, children}) => {
+export const CroppedIconWithAction = ({icon, alternateIcon, title, actionText, onClick, className, children}) => {
   return (
     <div
       tabIndex="0"
@@ -26,7 +26,7 @@ export const CroppedIconWithAction = ({icon, title, actionText, onClick, classNa
       className={className || ""}
     >
       <div className="-elv-cropped-icon -elv-cropped-icon-with-action">
-        <ImageIcon icon={icon} title={title} />
+        <ImageIcon icon={icon} alternateIcon={alternateIcon} title={title} />
         <div className="-elv-hover-action">
           <span>{actionText}</span>
           { children }
@@ -36,16 +36,23 @@ export const CroppedIconWithAction = ({icon, title, actionText, onClick, classNa
   );
 };
 
-export const ImageIcon = ({icon, title, className}) => {
+export const ImageIcon = ({icon, alternateIcon, title, className}) => {
+  const [error, setError] = React.useState(false);
+
   className = "-elv-icon " + (className || "");
 
-  if(icon.startsWith("<svg")) {
+  const currentIcon = error ? alternateIcon : icon;
+  const handleError = error ? undefined : () => setError(true);
+
+  if(!currentIcon) { return null; }
+
+  if(currentIcon.startsWith("<svg")) {
     return (
-      <InlineSVG title={title} alt={title} className={className} src={icon}/>
+      <InlineSVG title={title} alt={title} className={className} src={currentIcon}/>
     );
   } else {
     return (
-      <img title={title} alt={title} className={className} src={icon} />
+      <img title={title} alt={title} className={className} src={currentIcon} onError={handleError} />
     );
   }
 };
