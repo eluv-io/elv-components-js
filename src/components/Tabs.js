dc5194e5cafe4af1489613dc7e174cdf7a093fc5
Kevin Talmadge <kevin@eluv.io>
2019-08-14 12:59:32 -0700

    Add tab classname option to Tabs component

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -3,15 +3,12 @@ import "../stylesheets/tabs.scss";
 import React from "react";
 import PropTypes from "prop-types";
 
-const Tabs = ({options, selected, onChange, className=""}) => {
+const Tabs = ({options, selected, onChange, className="", tabClassName=""}) => {
   const tabs = options.map(([label, value]) => {
     return (
       <li
         tabIndex={0}
-        className={
-          "-elv-tab " + className +
-          (selected === value ? " selected" : "")
-        }
+        className={`-elv-tab ${tabClassName || ""} ${selected === value ? " selected" : ""}`}
         onClick={() => onChange(value)}
         onKeyPress={() => onChange(value)}
         key={"-elv-tab-" + value}
@@ -43,7 +40,8 @@ Tabs.propTypes = {
     PropTypes.number
   ]),
   onChange: PropTypes.func.isRequired,
-  className: PropTypes.string
+  className: PropTypes.string,
+  tabClassName: PropTypes.string
 };
 
 export default Tabs;
